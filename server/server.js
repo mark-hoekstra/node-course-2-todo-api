3008400393a361ae2986eb3645385f3a85031f4d
Mark Hoekstra <m.a.hoekstra@outlook.com>
2018-11-28 10:22:45 +0100

    updated test

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -25,9 +24,9 @@ app.get('/todos', (req, res) => {
     Todo.find().then((todos)=>{
         res.send({todos})
     }, (e) => {
-        res.status(400).send(e)
-    })
-})
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000')
