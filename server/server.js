6c8b5d44e04342bd15a4e6b05b142f3d00e8f509
Mark Hoekstra <m.a.hoekstra@outlook.com>
2018-11-29 10:29:25 +0100

    Add route through ID

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -8,28 +9,45 @@ var {User} = require('./models/user');
 var app = express();
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-    var todo = new Todo({
-        text: req.body.text
-    });
+// app.post('/todos', (req, res) => {
+//     var todo = new Todo({
+//         text: req.body.text
+//     });
 
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (err) => {
-        res.status(400).send(err);
-    });
-});
+//     todo.save().then((doc) => {
+//         res.send(doc);
+//     }, (err) => {
+//         res.status(400).send(err);
+//     });
+// });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos)=>{
-        res.send({todos})
-    }, (e) => {
-        res.status(400).send(e);
+// app.get('/todos', (req, res) => {
+//     Todo.find().then((todos)=>{
+//         res.send({todos})
+//     }, (e) => {
+//         res.status(400).send(e);
+//     });
+// });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(()=>{
+        res.status(400).send();
     });
 });
 
 app.listen(3000, () => {
-    console.log('Started on port 3000')
+    console.log('Started on port 3000');
 });
 
 module.exports = {app};
