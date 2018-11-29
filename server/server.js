3ff9b35648275e55c69389f428ca17cbcd800a5b
Mark Hoekstra <m.a.hoekstra@outlook.com>
2018-11-29 10:59:49 +0100

    Added tests for GET /todos/id

--- server/server.js
+++ server/server.js
@@ -9,25 +9,25 @@ var {User} = require('./models/user');
 var app = express();
 app.use(bodyParser.json());
 
-// app.post('/todos', (req, res) => {
-//     var todo = new Todo({
-//         text: req.body.text
-//     });
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
 
-//     todo.save().then((doc) => {
-//         res.send(doc);
-//     }, (err) => {
-//         res.status(400).send(err);
-//     });
-// });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
 
-// app.get('/todos', (req, res) => {
-//     Todo.find().then((todos)=>{
-//         res.send({todos})
-//     }, (e) => {
-//         res.status(400).send(e);
-//     });
-// });
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos)=>{
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
